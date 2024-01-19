b37b8deef36caff9366acfd4d508f2fe3e0659f7
Zoltus <zoltus@outlook.com>
2024-01-19 10:58:58 +0200

    Fix render.com route problems.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from "react";
-import {BrowserRouter as Router, Route, Routes, Link} from "react-router-dom";
+import {BrowserRouter as Router, Link, Route, Routes} from "react-router-dom";
 import ExercisesPage from "./pages/ExercisesPage.jsx";
 import CreateExercisePage from "./pages/CreateExercisePage.jsx";
 import List from '@mui/material/List';
@@ -7,6 +7,7 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
+import {Drawer} from "@mui/material";
 import AddIcon from '@mui/icons-material/Add';
 import Database from "./Database.jsx";
 import SchoolIcon from '@mui/icons-material/School';
@@ -27,29 +28,38 @@ function App() {
     return <>
         <Router>
             <div className="flex bg-gray-200">
-
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/CreateExercisePage">create</Link>
-                        </li>
-                    </ul>
-                </nav>
-
+                <Drawer
+                    anchor="left"
+                    variant="permanent"
+                    sx={{
+                        width: 240,
+                        flexShrink: 0,
+                        '& .MuiDrawer-paper': {width: 240},
+                    }}>
+                    <List>
+                        {[{text: 'Exercises', path: '/'},
+                            {text: 'Add Exercise', path: '/CreateExercisePage'},
+                        ].map((item, index) => (
+                            <ListItem key={item.text} disablePadding>
+                                <Link to={item.path} style={{ textDecoration: 'none', color: 'inherit' }}>
+                                    <ListItemButton>
+                                        <ListItemIcon>
+                                            {index % 2 === 0 ? <SchoolIcon /> : <AddIcon />}
+                                        </ListItemIcon>
+                                        <ListItemText primary={item.text} />
+                                    </ListItemButton>
+                                </Link>
+                            </ListItem>
+                        ))}
+                    </List>
+                </Drawer>
                 <div className="flex-1 flex-grow-1 mx-16">
-
                     <div className="flex h-screen">
                         <main className="flex-1 p-4">
                             <Routes>
-                                <Route path="/" element={<ExercisesPage exercises={exercises}
-                                                                        setCurrentExercise={setCurrentExercise}/>}/>
-                                <Route path="/CreateExercisePage" element={<CreateExercisePage exercises={exercises}
-                                                                                              setExercises={setExercises}/>}/>
-                                <Route path="/StudyPage" element={<StudyPage currentExercise={currentExercise}
-                                                                            setCurrentExercise={setCurrentExercise}/>}/>
+                                <Route path="/" element={<ExercisesPage exercises={exercises} setCurrentExercise={setCurrentExercise}/>}/>
+                                <Route path="/CreateExercisePage" element={<CreateExercisePage exercises={exercises} setExercises={setExercises}/>}/>
+                                <Route path="/StudyPage" element={<StudyPage currentExercise={currentExercise} setCurrentExercise={setCurrentExercise}/>} />
                             </Routes>
                         </main>
                     </div>
