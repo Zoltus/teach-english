80c720b5aab67d18a76244017bb434cb453f6a0d
Zoltus <zoltus@outlook.com>
2024-01-19 10:40:40 +0200

    Try to fix router to work on render.com

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from "react";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes, Link} from "react-router-dom";
 import ExercisesPage from "./pages/ExercisesPage.jsx";
 import CreateExercisePage from "./pages/CreateExercisePage.jsx";
 import List from '@mui/material/List';
@@ -7,7 +7,6 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import {Drawer, Link} from "@mui/material";
 import AddIcon from '@mui/icons-material/Add';
 import Database from "./Database.jsx";
 import SchoolIcon from '@mui/icons-material/School';
@@ -26,44 +25,37 @@ function App() {
     }, []);
 
     return <>
-        <BrowserRouter>
+        <Router>
             <div className="flex bg-gray-200">
-                <Drawer
-                    anchor="left"
-                    variant="permanent"
-                    sx={{
-                        width: 240,
-                        flexShrink: 0,
-                        '& .MuiDrawer-paper': {width: 240},
-                    }}>
-                    <List>
-                        {[{text: 'Exercises', path: '/'},
-                            {text: 'Add Exercise', path: '/CreateExercisePage'},
-                        ].map((item, index) => (
-                            <ListItem key={item.text} disablePadding>
-                                <ListItemButton component={Link} to={item.path}>
-                                    <ListItemIcon>
-                                        {index % 2 === 0 ? <SchoolIcon/> : <AddIcon/>}
-                                    </ListItemIcon>
-                                    <ListItemText primary={item.text}/>
-                                </ListItemButton>
-                            </ListItem>
-                        ))}
-                    </List>
-                </Drawer>
+
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/CreateExercisePage">create</Link>
+                        </li>
+                    </ul>
+                </nav>
+
                 <div className="flex-1 flex-grow-1 mx-16">
+
                     <div className="flex h-screen">
                         <main className="flex-1 p-4">
                             <Routes>
-                                <Route path="/" element={<ExercisesPage exercises={exercises} setCurrentExercise={setCurrentExercise}/>}/>
-                                <Route path="/CreateExercisePage" element={<CreateExercisePage exercises={exercises} setExercises={setExercises}/>}/>
-                                <Route path="/StudyPage" element={<StudyPage currentExercise={currentExercise} setCurrentExercise={setCurrentExercise}/>} />
+                                <Route path="/" element={<ExercisesPage exercises={exercises}
+                                                                        setCurrentExercise={setCurrentExercise}/>}/>
+                                <Route path="/CreateExercisePage" element={<CreateExercisePage exercises={exercises}
+                                                                                              setExercises={setExercises}/>}/>
+                                <Route path="/StudyPage" element={<StudyPage currentExercise={currentExercise}
+                                                                            setCurrentExercise={setCurrentExercise}/>}/>
                             </Routes>
                         </main>
                     </div>
                 </div>
             </div>
-        </BrowserRouter>
+        </Router>
     </>
 }
 
